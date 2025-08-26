3339b64912f7a48d8d1fee9b3af2da3acf0f36d8
Junior Herrera <herrerajunior715@gmail.com>
2025-08-26 14:31:38 -0500

    feat(feedback): mejoras de estilos y feedback visual en SuccessMessage y quejas

--- frontend/src/pages/components/feedbackform/SuccessMessage.tsx
+++ frontend/src/pages/components/feedbackform/SuccessMessage.tsx
@@ -1,15 +1,22 @@
 import "./styles/Complain.css";
 import './styles/SuccesMessage.css'
+import { useNavigationHandler } from "../../../hooks/useNavigationHandler";
+import { TbXboxX } from 'react-icons/tb';
+
 interface Props {
   onReset: () => void;
 }
 
 const SuccessMessage: React.FC<Props> = ({ onReset }) => {
+    const handleBtnNavigate = useNavigationHandler()
+
   return (
     <div className="success-message">
+      <button className="btn-back-message" onClick={()=>handleBtnNavigate('/back')}>{<TbXboxX/>}</button>
+
       <h2>¡Gracias por tu mensaje!</h2>
       <p>Tu queja ha sido registrada correctamente.</p>
-      <button onClick={onReset}>Enviar otra</button>
+      <button onClick={onReset} className="btn-send-other-sugerence">Enviar otra</button>
     </div>
   );
 };
